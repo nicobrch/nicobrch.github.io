0dfa275ce3b2d67e1ca90d41e3efef17c6829070
Nico <nicolas.brch@gmail.com>
2024-04-16 23:20:47 -0400

    Generate build using any types :(

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -2,14 +2,14 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import { Suspense } from 'react';
 
 const components = {
-  h1: (props) => (
+  h1: (props: any) => (
     <h1 {...props} className="text-2xl font-medium">
       {props.children}
     </h1>
   )
 }
 
-export function CustomMDX(props) {
+export function CustomMDX(props: any) {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <MDXRemote
